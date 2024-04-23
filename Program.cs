7e4dc572c88dcd89a1a9742e4c32b3293f266847
Aidar <sharipovaydar200515@gmail.com>
2024-04-23 11:50:59 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+
+// Класс парсер
+class ObjParser
+{
+    // Статический метод (Не обязательно создавать экземпляр класса для его вызова) читает файл
+    public static void getAllText()
+    {
+        //Путь к файлу
+        string filePath = "../../../sword/sword.obj";
+
+        // Каждая строка файла отдельный элемент в списке
+        string[] lines = File.ReadAllLines(filePath);
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+        Console.ReadLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ObjParser.getAllText();
+    }
+}
+
