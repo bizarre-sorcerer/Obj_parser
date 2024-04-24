aa0a4a718f4ba31c5a719b024dadfc5695804e48
Aidar <sharipovaydar200515@gmail.com>
2024-04-24 18:57:43 +0600

    Организовал код под классом в свойства и несколько методов. После парсинга файла Parse() извлекает линии в отдельные списки в зависимости от токена в начале линии(v/vt/vn/f)

--- Program.cs
+++ Program.cs
@@ -5,23 +5,72 @@ using System.Security.Cryptography.X509Certificates;
 
 
 // Класс парсер
+// В метод Parse(filePath) дается путь к .obj файлу. Он его парсит и извлекает линии в отдельные списки в зависимости от токена в начале линии(v/vt/vn/f)
+// Списки не возвращаются, а храняться статически как свойства класса
 class ObjParser
 {
-    // Статический метод (Не обязательно создавать экземпляр класса для его вызова) читает файл
-    public static void getAllText()
+    // Координаты всех вершин треугольников
+    public static List<string> vertexes = new List<string>();
+
+    // Координаты текстур
+    public static List<string> textures = new List<string>();
+
+    // Нормали
+    public static List<string> normals = new List<string>();
+
+    // Лица треугольников
+    public static List<string> faces = new List<string>();
+
+    // Возвращает список где каждая линия файла это элемент списка
+    private static string[] getAllText(string filePath)
     {
-        //Путь к файлу
-        string filePath = "../../../sword/sword.obj";
+        // Каждая строка файла отдельный элемент в массиве
+        string[] linesArray = File.ReadAllLines(filePath);
 
-        // Каждая строка файла отдельный элемент в списке
-        string[] lines = File.ReadAllLines(filePath);
+        return linesArray;
+    }
 
+    // Сортирует линии по отдельным спискам. Вершины в одном списке, нормали в одной списке и т.д
+    private static void sortLines(string[] lines)
+    {
+        // Проверяeт каждую линию и добавляет ее в соответстующий список в зависимости от токена(v/vt и т.д)
         foreach (string line in lines)
         {
-            Console.WriteLine(line);
+            // Вершина 
+            if (line[0] == 'v' && line[1] == ' ')
+            {
+                vertexes.Add(line);
+            }
+            // Текстура
+            else if (line[0] == 'v' && line[1] == 't')
+            {
+                textures.Add(line);
+            }
+            // Нормаль
+            else if (line[0] == 'v' && line[1] == 'n')
+            {
+                normals.Add(line);
+            }
+            // Лицо
+            else if (line[0] == 'f')
+            {
+                faces.Add(line);
+            }
         }
+    }
 
-        Console.ReadLine();
+    // Это будет финальной функций которая будет делать все сразу.
+    // Парсит файл, вытаскивает все нужные данные и организовывает их формате, который понимают движки для 3д рендеринга
+    public static void Parse(string filePath)
+    {
+        // Получаем список со всеми линиями текста
+        string[] lines = getAllText(filePath);
+
+        // Запихиваем из по спискам
+        sortLines(lines);
+
+        // TODO
+        // Огранизует в тип данных который понятен для движков 3д рендеринга
     }
 }
 
@@ -29,7 +78,18 @@ class Program
 {
     static void Main()
     {
-        ObjParser.getAllText();
+        // Путь к файлу
+        string filePath = "../../../sword/sword.obj";
+
+        // Парсит
+        ObjParser.Parse(filePath);
+
+        // Просто выводим в консоль чтобы убедить что работает как задуманно
+        foreach (string item in ObjParser.normals)
+        {
+            Console.WriteLine(item);
+        }
+        Console.ReadLine();
     }
 }
 
